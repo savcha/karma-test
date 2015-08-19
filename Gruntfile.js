560faa24fa635b7e6720a297f66401919dfd319a
savcha <anton.savchenko@studentuniverse.com>
2015-08-19 09:57:15 -0400

    base

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-npm-install');
+
+  // Default task(s).
+  grunt.registerTask('default', ['npm-install', 'karma']);
+
+};
---
+++ dummy-test.js
@@ -0,0 +1,5 @@
+describe("A dummy test", function(){
+    it("contains spec wit an expectation", function(){
+	expect(true).toBe(true);
+    });
+});
